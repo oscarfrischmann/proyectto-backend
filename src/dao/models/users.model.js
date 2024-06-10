10b7660c83f33ef557bcacc88dd1eca704730fca
Oscar Frischmann <oscarfrisc@gmail.com>
2024-06-10 12:55:15 -0300

    Aggregate!

--- src/dao/models/users.model.js
+++ src/dao/models/users.model.js
@@ -2,14 +2,36 @@ import mongoose from 'mongoose';
 
 mongoose.pluralize(null);
 
-const collection = 'users';
+const collection = 'users_aggregate';
 
 const schema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true, index: true },
 	email: { type: String, required: true },
 	password_: { type: String, required: true },
-	role: { type: String, enum: ['user', 'premium', 'admin'], default: 'user' },
+	// role: { type: String, enum: ['user', 'premium', 'admin'], default: 'user' },
+	role: { type: String, enum: ['Male', 'Female', 'Bigender'], default: 'Male' },
+	grade: { type: Number, required: true },
+	region: {
+		type: String,
+		enum: [
+			'United States',
+			'Sweden',
+			'Indonesia',
+			'Lithuania',
+			'Poland',
+			'China',
+			'Netherlands',
+			'Dominican Republic',
+			'Greece',
+			'Israel',
+			'Egypt',
+			'Russia',
+			'Tanzania',
+			'Thailand',
+		],
+		default: 'China',
+	},
 });
 
 const model = mongoose.model(collection, schema);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,12 +6,25 @@ const router = Router();
 router.get('/db/users', async (req, res) => {
 	try {
 		// const usersDB = await usersModel.find().lean();
-		const usersDB = await usersModel.find({ lastName: 'Fahey' }).explain('executionStats');
+		const usersDB = await usersModel.find().lean(); //.explain('executionStats')//;
 
 		console.log(await usersModel.countDocuments());
 		res.status(200).send({ origin: 'get from DB users', payload: usersDB });
 	} catch (err) {
 		res.status(500).send({ origin: 'get from DB users', payload: err });
+	}
+});
+
+router.get('/db/aggreg', async (req, res) => {
+	try {
+		const process = await usersModel.aggregate([
+			{ $match: { role: 'Male' } },
+			{ $group: { _id: '$region', totalGrade: { $sum: '$grade' } } },
+			{ $sort: { totalGrade: -1 } },
+		]);
+		res.status(200).send({ origin: 'get aggregate', payload: process });
+	} catch (err) {
+		res.status(500).send({ origin: 'get aggregate', payload: err });
 	}
 });
 
