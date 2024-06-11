cb7e1a87ddbfc0a6078439670cd0e9fca3ae1275
Oscar Frischmann <oscarfrisc@gmail.com>
2024-06-11 09:47:56 -0300

    paginate

--- src/dao/models/users.model.js
+++ src/dao/models/users.model.js
@@ -1,38 +1,41 @@
 import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
 
 mongoose.pluralize(null);
 
-const collection = 'users_aggregate';
+const collection = 'users';
 
 const schema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true, index: true },
 	email: { type: String, required: true },
 	password_: { type: String, required: true },
-	// role: { type: String, enum: ['user', 'premium', 'admin'], default: 'user' },
-	role: { type: String, enum: ['Male', 'Female', 'Bigender'], default: 'Male' },
-	grade: { type: Number, required: true },
-	region: {
-		type: String,
-		enum: [
-			'United States',
-			'Sweden',
-			'Indonesia',
-			'Lithuania',
-			'Poland',
-			'China',
-			'Netherlands',
-			'Dominican Republic',
-			'Greece',
-			'Israel',
-			'Egypt',
-			'Russia',
-			'Tanzania',
-			'Thailand',
-		],
-		default: 'China',
-	},
+	role: { type: String, enum: ['user', 'premium', 'admin'], default: 'user' },
+	// role: { type: String, enum: ['Male', 'Female', 'Bigender'], default: 'Male' },
+	// grade: { type: Number, required: true },
+	// region: {
+	// 	type: String,
+	// 	enum: [
+	// 		'United States',
+	// 		'Sweden',
+	// 		'Indonesia',
+	// 		'Lithuania',
+	// 		'Poland',
+	// 		'China',
+	// 		'Netherlands',
+	// 		'Dominican Republic',
+	// 		'Greece',
+	// 		'Israel',
+	// 		'Egypt',
+	// 		'Russia',
+	// 		'Tanzania',
+	// 		'Thailand',
+	// 	],
+	// 	default: 'China',
+	// },
 });
+
+schema.plugin(mongoosePaginate);
 
 const model = mongoose.model(collection, schema);
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,7 +6,8 @@ const router = Router();
 router.get('/db/users', async (req, res) => {
 	try {
 		// const usersDB = await usersModel.find().lean();
-		const usersDB = await usersModel.find().lean(); //.explain('executionStats')//;
+		// const usersDB = await usersModel.find().skip(5).limit(5).lean(); //.explain('executionStats')//;
+		const usersDB = await usersModel.find().lean();
 
 		console.log(await usersModel.countDocuments());
 		res.status(200).send({ origin: 'get from DB users', payload: usersDB });
@@ -28,4 +29,13 @@ router.get('/db/aggreg', async (req, res) => {
 	}
 });
 
+router.get('/db/paginate/:role', async (req, res) => {
+	const role = req.params.role;
+	try {
+		const process = await usersModel.paginate({}, { limit: 50, page: 2 });
+		res.status(200).send({ origin: 'get paginate', payload: process });
+	} catch (err) {
+		res.status(500).send({ origin: 'get paginate', payload: err });
+	}
+});
 export default router;
