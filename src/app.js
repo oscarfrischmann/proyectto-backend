6b49df7b3d11d7221c5037d47562133a3104ee14
Oscar Frischmann <oscarfrisc@gmail.com>
2024-06-11 19:39:11 -0300

    websockets with DB

--- src/app.js
+++ src/app.js
@@ -1,49 +1,51 @@
-import express, { urlencoded } from 'express';
-import handlebars from 'express-handlebars';
-import mongoose from 'mongoose';
-import { Server } from 'socket.io';
-import config from './config.js';
-import cartsRouter from './routes/carts.routes.js';
-import productsRouter from './routes/products.routes.js';
-import viewsRouter from './routes/views.routes.js';
-import usersRouter from './routes/users.routes.js';
+import express, { urlencoded } from "express";
+import handlebars from "express-handlebars";
+import mongoose from "mongoose";
+import { Server } from "socket.io";
+import config from "./config.js";
+import cartsRouter from "./routes/carts.routes.js";
+import productsRouter from "./routes/products.routes.js";
+import viewsRouter from "./routes/views.routes.js";
+import usersRouter from "./routes/users.routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-app.use('/static', express.static(`${config.DIRNAME}/public`));
+app.use("/static", express.static(`${config.DIRNAME}/public`));
 
-app.engine('handlebars', handlebars.engine());
-app.set('views', `${config.DIRNAME}/views`);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", handlebars.engine());
+app.set("views", `${config.DIRNAME}/views`);
+app.set("view engine", "handlebars");
 
 //* views
-app.use('/', viewsRouter);
+app.use("/", viewsRouter);
 
 //*products
 
-app.use('/', productsRouter);
-app.use('/', usersRouter);
+app.use("/", productsRouter);
+app.use("/", usersRouter);
 
-app.use('/static', express.static(`${config.DIRNAME}/public`));
+app.use("/static", express.static(`${config.DIRNAME}/public`));
 
 //*carts
 
-app.use('/', cartsRouter);
+app.use("/", cartsRouter);
 
 const httpServer = app.listen(config.PORT, async () => {
-	await mongoose.connect(config.ATLAS_URI);
-	console.log(`App activa en puerto ${config.PORT} enlazada a DB remota`);
+  await mongoose.connect(config.ATLAS_URI);
+  console.log(`App activa en puerto ${config.PORT} enlazada a DB remota`);
 });
 
 const socketServer = new Server(httpServer);
-app.set('socketServer', socketServer);
+app.set("socketServer", socketServer);
 //escucho evento tipo conxión
-socketServer.on('connection', (client) => {
-	console.log(`Cliente conectado, id ${client.id} desde ${client.handshake.address}`);
-	client.on('newProduct', () => {
-		console.log('newProduct function on');
-		socketServer.emit('newProductConfirmation', 'OkOKOK');
-	});
+socketServer.on("connection", (client) => {
+  console.log(
+    `Cliente conectado, id ${client.id} desde ${client.handshake.address}`
+  );
+  client.on("newProduct", () => {
+    console.log("newProduct function on");
+    socketServer.emit("newProductConfirmation", "OkOKOK");
+  });
 });
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,97 +1,102 @@
-import { Router } from 'express';
-import config from '../config.js';
-import productManager from '../productManager.js';
-import productsModel from '../dao/models/products.model.js';
+import { Router } from "express";
+import config from "../config.js";
+import productManager from "../productManager.js";
+import productsModel from "../dao/models/products.model.js";
 
 const router = Router();
 
 //* ATLAS MONGOOSE
-router.get('/db/products', async (req, res) => {
-	try {
-		const productsDB = await productsModel.find().lean();
-		console.log(await productsModel.countDocuments());
-		res.status(200).send({ origin: 'get from DB', payload: productsDB });
-	} catch (e) {
-		res.status(500).send({ origin: 'get from DB', payload: null, error: e });
-	}
+router.get("/db/products", async (req, res) => {
+  try {
+    const productsDB = await productsModel.find().lean();
+    console.log(await productsModel.countDocuments());
+    res.status(200).send({ origin: "get from DB", payload: productsDB });
+  } catch (e) {
+    res.status(500).send({ origin: "get from DB", payload: null, error: e });
+  }
 });
 
-router.get('/db/products/one/:id', async (req, res) => {
-	try {
-		const productDB = await productsModel.findById(req.params.id);
-		res.status(200).send({ origin: 'get one from DB ', payload: productDB });
-	} catch (e) {
-		res.status(500).send({ origin: 'get from DB', payload: null, error: e });
-	}
+router.get("/db/products/:id", async (req, res) => {
+  try {
+    const productDB = await productsModel.findById(req.params.id);
+    res.status(200).send({ origin: "get one from DB ", payload: productDB });
+  } catch (e) {
+    res.status(500).send({ origin: "get from DB", payload: null, error: e });
+  }
 });
 
-router.post('/db/products', async (req, res) => {
-	try {
-		const newProduct = req.body;
-		await productsModel.create(newProduct);
-		res.status(200).send({ origin: 'post one from DB ', payload: newProduct });
-	} catch (e) {
-		res.status(500).send({ origin: 'post from DB', payload: null, error: e });
-	}
+router.post("/db/products", async (req, res) => {
+  try {
+    const socketServer = req.app.get("socketServer");
+    const newProduct = req.body;
+    await productsModel.create(newProduct);
+    socketServer.emit("newProductConfirmation", newProduct);
+
+    res.status(200).send({ origin: "post one from DB ", payload: newProduct });
+  } catch (e) {
+    res.status(500).send({ origin: "post from DB", payload: null, error: e });
+  }
 });
 
 //* FILE SYSTEM
 const manager = new productManager(`${config.DIRNAME}/products.json`);
 
-router.get('/api/products', (req, res) => {
-	const limit = req.query.limit;
-	manager.getProducts(limit).then((data) => {
-		res.status(200).send({ origin: 'getLimit', payload: data });
-	});
+router.get("/api/products", (req, res) => {
+  const limit = req.query.limit;
+  manager.getProducts(limit).then((data) => {
+    res.status(200).send({ origin: "getLimit", payload: data });
+  });
 });
 
-router.get('/api/product/:pid', (req, res) => {
-	const { pid } = req.params;
-	console.log(pid);
-	manager.getProductsById(parseInt(pid)).then((data) => {
-		res.status(200).send({ origin: 'getID', payload: data });
-	});
+router.get("/api/product/:pid", (req, res) => {
+  const { pid } = req.params;
+  console.log(pid);
+  manager.getProductsById(parseInt(pid)).then((data) => {
+    res.status(200).send({ origin: "getID", payload: data });
+  });
 });
 
-router.post('/api/product', (req, res) => {
-	//:title/:description/:price/:thumbnail/:code/:stock
-	const socketServer = req.app.get('socketServer');
-	const newProduct = req.body;
-	const parse = { ...newProduct };
-	manager.addProduct(parse);
-	socketServer.emit('newProductConfirmation', newProduct);
+router.post("/api/product", (req, res) => {
+  //:title/:description/:price/:thumbnail/:code/:stock
+  const socketServer = req.app.get("socketServer");
+  const newProduct = req.body;
+  const parse = { ...newProduct };
+  manager.addProduct(parse);
+  socketServer.emit("newProductConfirmation", newProduct);
 
-	res.status(200).send({ origin: 'post', payload: parse });
+  res.status(200).send({ origin: "post", payload: parse });
 });
 
-router.put('/:sid', (req, res) => {
-	const { sid } = req.params;
-	const id = +sid;
-	const { description = '', title = '', stock, price } = req.body;
-	manager.getProductsById(id).then((oldData) => {
-		console.log(req.body);
-		if (oldData === 'No such ID found') {
-			res.status(400).send({ origin: 'put', payload: {}, error: 'ID inexistente' });
-		} else {
-			const updated = { ...oldData, ...req.body };
-			manager.updateProduct(id, req.body);
-			res.status(200).send({ origin: 'put', payload: req.body, body: updated });
-		}
-	});
+router.put("/:sid", (req, res) => {
+  const { sid } = req.params;
+  const id = +sid;
+  const { description = "", title = "", stock, price } = req.body;
+  manager.getProductsById(id).then((oldData) => {
+    console.log(req.body);
+    if (oldData === "No such ID found") {
+      res
+        .status(400)
+        .send({ origin: "put", payload: {}, error: "ID inexistente" });
+    } else {
+      const updated = { ...oldData, ...req.body };
+      manager.updateProduct(id, req.body);
+      res.status(200).send({ origin: "put", payload: req.body, body: updated });
+    }
+  });
 });
 
-router.delete('/api/product/:sid', (req, res) => {
-	const { sid } = req.params;
-	const id = +sid;
-	manager
-		.deleteProduct(id)
-		.then(() => {
-			res.status(200).send(`Produto ${id} removido`);
-		})
-		.catch((err) => {
-			console.log('Error no delete: ', err);
-			res.status(500).send(`No se puede borrar el producto con ID "${id}"`);
-		});
+router.delete("/api/product/:sid", (req, res) => {
+  const { sid } = req.params;
+  const id = +sid;
+  manager
+    .deleteProduct(id)
+    .then(() => {
+      res.status(200).send(`Produto ${id} removido`);
+    })
+    .catch((err) => {
+      console.log("Error no delete: ", err);
+      res.status(500).send(`No se puede borrar el producto con ID "${id}"`);
+    });
 });
 
 export default router;
